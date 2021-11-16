b0836c5a4fe5f278cc2bceacc097ded792c4485f
eleung32 <52718704+eleung32@users.noreply.github.com>
2021-11-16 15:03:54 -0500

    adjusted layout and server.js house keeping

--- client/src/components/ImageLinkForm/ImageLinkForm.js
+++ client/src/components/ImageLinkForm/ImageLinkForm.js
@@ -4,13 +4,13 @@ import './ImageLinkForm.css';
 const ImageLinkForm = ({ onInputChange, onInputChange2, onButtonSubmit }) => {
   return (
     <div>
-      <p className='f1'>
+      <p className='f1 pt4'>
         {'Feeling hungry?'}
       </p>
       <p className='f3'>
         {'Please enter your area and type of food you are looking for'}
       </p>
-      <div className='center'>
+      <div className='center pt3'>
         <div className='form pa4 br3 shadow-5'>
           <input  className='f4 pa2 w-50' 
                   type='text'
@@ -19,7 +19,7 @@ const ImageLinkForm = ({ onInputChange, onInputChange2, onButtonSubmit }) => {
           <input className='f4 pa2 w-80'
                  type='text'
                  placeholder='type of cuisine, restaurant name or street name'
-                 onChange={onInputChange2}/>  
+                 onChange={onInputChange2}/>           
           <button
             className='w-28 grow f4 link ph3 pv2 dib white bg-light-red'
             onClick={onButtonSubmit}
--- server/src/server.js
+++ server/src/server.js
@@ -1,35 +1,15 @@
 const express = require('express');
-// const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-// const knex = require('knex');
-
-// const register = require('./controllers/register');
-// const signin = require('./controllers/signin');
-// const profile = require('./controllers/profile');
 const image = require('./controllers/restaurant');
-
-// const db = knex({
-//   client: 'pg',
-//   connection: {
-//     host : '127.0.0.1',
-//     user : 'postgres',
-//     password : 'admin',
-//     database : 'smart-brain'
-//   }
-// });
-
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=> {res.send("LOOK ITS WORKING") })
-// app.post('/signin', signin.handleSignin(db, bcrypt))
-// app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-// app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.post('/search', (req, res) => { image.handleApiCall(req, res)})
+app.get('/', (req, res)=> {res.send("LOOK ITS WORKING") });
+app.post('/search', (req, res) => { image.handleApiCall(req, res)});
 
 app.listen(PORT, ()=> {
   console.log(`listening on port ${PORT}...`);
-})
+});
