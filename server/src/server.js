b72433a11d0430a9b865e1cdc3da7cefde80936d
eleung32 <52718704+eleung32@users.noreply.github.com>
2021-11-17 15:40:59 -0500

    modified server.js to serve docker at port 8000

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,27 @@
-const express = require('express');
+const path = require('path');
 const cors = require('cors');
+const express = require('express');
 const image = require('./controllers/restaurant');
+// const morgan = require('morgan');
+
 const app = express();
+
 const PORT = process.env.PORT || 8000;
 
+// app.use(cors({
+//   origin: 'http://localhost:3000',
+// }));
 app.use(cors());
-app.use(express.json());
+// app.use(morgan('combined'));
 
-app.get('/', (req, res)=> {res.send("LOOK ITS WORKING") });
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// app.get('/', (req, res)=> {res.send("LOOK ITS WORKING") });
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
 app.post('/search', (req, res) => { image.handleApiCall(req, res)});
 
 app.listen(PORT, ()=> {
