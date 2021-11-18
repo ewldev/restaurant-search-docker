ca37282db13b7b42cb879ce8c477a744bd92ea37
eleung32 <52718704+eleung32@users.noreply.github.com>
2021-11-18 00:27:41 -0500

    docker build and run working

--- server/src/server.js
+++ server/src/server.js
@@ -1,18 +1,19 @@
-const path = require('path');
-const cors = require('cors');
 const express = require('express');
+const path = require('path');
+
+const cors = require('cors');
 const image = require('./controllers/restaurant');
-// const morgan = require('morgan');
+const morgan = require('morgan');
 
 const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-// app.use(cors({
-//   origin: 'http://localhost:3000',
-// }));
-app.use(cors());
-// app.use(morgan('combined'));
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
+
+app.use(morgan('combined'));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
